bb2800a20e5e6d72cf0bafdacd7e03e58b811377
ShanksSGV <ndanny2@gmail.com>
2015-03-23 15:47:35 +0100

    added i18n compatibility

---
+++ plugins/objects/energyathome-things/src/main/java/com/freedomotic/things/impl/SmartPlug.java
@@ -0,0 +1,61 @@
+/**
+ *
+ * Copyright (c) 2014 Telecom Italia
+ *
+ * @author: Ing. Danny Noferi, SI.IIR.OIR, Joint Open Lab S-Cube
+ *
+ */
+package com.freedomotic.things.impl;
+
+import com.freedomotic.model.object.BooleanBehavior;
+import com.freedomotic.reactions.Command;
+import static com.freedomotic.things.impl.ElectricDevice.BEHAVIOR_POWERED;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author Danny
+ */
+public class SmartPlug extends ElectricDevice {
+    
+    //partial implementation of i18n... waiting for future improvements
+    
+    Locale it = new Locale("it","IT");
+    ResourceBundle messages = ResourceBundle.getBundle("data/i18n/energyathomethings", it);
+
+    @Override
+    public void init() {
+        super.init();
+    }
+
+    @Override
+    protected void createCommands() {
+        super.createCommands();
+        
+        //i18n commands
+        
+        Command turnon = new Command();
+        turnon.setName(messages.getString("turnon") + getPojo().getName());
+        turnon.setReceiver("app.events.sensors.behavior.request.objects");
+        turnon.setProperty("object", getPojo().getName());
+        turnon.setProperty("behavior", BEHAVIOR_POWERED);
+        turnon.setProperty("value", BooleanBehavior.VALUE_TRUE);
+        
+        commandRepository.create(turnon);
+
+        Command turnoff = new Command();
+        turnoff.setName(messages.getString("turnoff") + getPojo().getName());
+        turnoff.setReceiver("app.events.sensors.behavior.request.objects");
+        turnoff.setProperty("object", getPojo().getName());
+        turnoff.setProperty("behavior", BEHAVIOR_POWERED);
+        turnoff.setProperty("value", BooleanBehavior.VALUE_FALSE);
+        
+        commandRepository.create(turnoff);
+    }
+
+    @Override
+    protected void createTriggers() {
+        super.createTriggers();
+    }
+}
